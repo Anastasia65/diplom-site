06018f82228f4d0d4552b03487a535ee652cbd4a
Anastasia65 <anst03@mail.ru>
2023-06-26 04:23:37 +0300

    добавлена метрика и политика

--- mail.php
+++ mail.php
@@ -22,14 +22,14 @@ $phone = trim($phone);
 $comment = trim($comment);
 
 //отправка данных на почту
-if (mail("t.pchela66@yandex.ru",
+if (mail("anst03@yandex.ru",
     "Новое письмо с сайта",
     "Имя: ".$name."\n". 
     "Телефон: ".$phone."\n". 
     "Комментарий: ".$comment,
-    "From: no-reply@mydomain.ru \r\n")
+    "From: no-reply@sadovaya-mebel.site \r\n")
     ) {
-        echo ('Отправлено');
+        echo ('<h3>Заявка отправлена</h3><br><a href="index.html#feedback">Вернуться назад</a>');
     }
     else {
         echo ('Есть ошибки');
